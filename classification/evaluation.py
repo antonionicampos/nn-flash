647471e502b4e0d8a31fe3f8fd86efcc992d577c
Antonioni Campos <antonioni.campos@petrobras.com>
2024-07-16 17:35:23 -0300

    improve roc curves

--- classification/evaluation.py
+++ classification/evaluation.py
@@ -88,65 +88,69 @@ def performance_indices(results: List[List[Dict[str, Any]]], valid_files: List[s
 def roc_analysis(
     results: List[List[Dict[str, Any]]],
     valid_files: List[str],
-    label: int,
     model_id: int,
-    figsize: Tuple[int] = (6, 6),
+    figsize: Tuple[int] = (15, 5),
 ):
-    n_splits = len(results[model_id])
-    tprs = []
-    aucs = []
-    mean_fpr = np.linspace(0, 1, 100)
+    n_folds = len(results[model_id])
 
-    f, ax = plt.subplots(figsize=figsize)
-    for fold, data in enumerate(results[model_id]):
-        model = data["model"]
+    f, axs = plt.subplots(1, 3, figsize=figsize)
 
-        data_file = valid_files[fold]
-        y, y_hat = binary_classification(model, data_file, label=label)
+    for label in range(3):
+        tprs = []
+        aucs = []
+        mean_fpr = np.linspace(0, 1, 100)
 
-        viz = RocCurveDisplay.from_predictions(
-            y,
-            y_hat,
-            name=f"ROC fold {fold + 1}",
-            alpha=0.3,
-            lw=1,
-            ax=ax,
-            plot_chance_level=(fold == n_splits - 1),
+        for fold, data in enumerate(results[model_id]):
+            model = data["model"]
+
+            data_file = valid_files[fold]
+            y, y_hat = binary_classification(model, data_file, label=label)
+
+            viz = RocCurveDisplay.from_predictions(
+                y,
+                y_hat,
+                name=f"ROC fold {fold + 1}",
+                alpha=0.3,
+                lw=1,
+                ax=axs[label],
+                plot_chance_level=(fold == n_folds - 1),
+            )
+            interp_tpr = np.interp(mean_fpr, viz.fpr, viz.tpr)
+            interp_tpr[0] = 0.0
+            tprs.append(interp_tpr)
+            aucs.append(viz.roc_auc)
+
+        mean_tpr = np.mean(tprs, axis=0)
+        mean_tpr[-1] = 1.0
+        mean_auc = auc(mean_fpr, mean_tpr)
+        std_auc = np.std(aucs)
+        axs[label].step(
+            mean_fpr,
+            mean_tpr,
+            color="b",
+            label=r"Mean ROC (AUC = %0.3f $\pm$ %0.3f)" % (mean_auc, std_auc),
+            lw=2,
+            alpha=0.8,
         )
-        interp_tpr = np.interp(mean_fpr, viz.fpr, viz.tpr)
-        interp_tpr[0] = 0.0
-        tprs.append(interp_tpr)
-        aucs.append(viz.roc_auc)
 
-    mean_tpr = np.mean(tprs, axis=0)
-    mean_tpr[-1] = 1.0
-    mean_auc = auc(mean_fpr, mean_tpr)
-    std_auc = np.std(aucs)
-    ax.plot(
-        mean_fpr,
-        mean_tpr,
-        color="b",
-        label=r"Mean ROC (AUC = %0.3f $\pm$ %0.3f)" % (mean_auc, std_auc),
-        lw=2,
-        alpha=0.8,
-    )
+        std_tpr = np.std(tprs, axis=0)
+        tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+        tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+        axs[label].fill_between(
+            mean_fpr,
+            tprs_lower,
+            tprs_upper,
+            color="grey",
+            alpha=0.2,
+            label=r"$\pm 1 \sigma$",
+        )
 
-    std_tpr = np.std(tprs, axis=0)
-    tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
-    tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
-    ax.fill_between(
-        mean_fpr,
-        tprs_lower,
-        tprs_upper,
-        color="grey",
-        alpha=0.2,
-        label=r"$\pm 1 \sigma$",
-    )
-
-    ax.set(
-        xlabel="False Positive Rate",
-        ylabel="True Positive Rate",
-        title=f"Mean ROC curve with variability\n(Positive label '{TARGET_NAMES[label]}')",
-    )
-    ax.legend(loc="lower right", prop={"size": 8})
+        axs[label].set(
+            xlabel="False Positive Rate",
+            ylabel="True Positive Rate",
+            title=f"Mean ROC curve with variability\n(Positive label '{TARGET_NAMES[label]}')",
+        )
+        axs[label].legend(loc="lower right", prop={"size": 8})
+        axs[label].grid()
+    f.tight_layout()
     plt.show()

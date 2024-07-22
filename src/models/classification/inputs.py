26a58a6897b9e41d95d1054a64efc70431070204
Antonioni Campos <antonioni.campos@petrobras.com>
2024-07-22 20:40:07 -0300

    activations as strings

--- src/models/classification/inputs.py
+++ src/models/classification/inputs.py
@@ -1,85 +1,79 @@
 import tensorflow as tf
+import uuid
 
 hparams = [
     {
-        "id": 1,
         "model_name": "Modelo #1",
         "arch": {
             "hidden_units": [],
-            "activation": tf.keras.activations.relu,
+            "activation": "relu",
         },
         "opt": {"lr": 0.001, "epochs": 500, "batch_size": 32},
     },
     {
-        "id": 2,
         "model_name": "Modelo #2",
         "arch": {
             "hidden_units": [32],
-            "activation": tf.keras.activations.relu,
+            "activation": "relu",
         },
         "opt": {"lr": 0.001, "epochs": 500, "batch_size": 32},
     },
     {
-        "id": 3,
         "model_name": "Modelo #3",
         "arch": {
             "hidden_units": [128],
-            "activation": tf.keras.activations.relu,
+            "activation": "relu",
         },
         "opt": {"lr": 0.001, "epochs": 500, "batch_size": 32},
     },
     {
-        "id": 4,
         "model_name": "Modelo #4",
         "arch": {
             "hidden_units": [32, 32],
-            "activation": tf.keras.activations.relu,
+            "activation": "relu",
         },
         "opt": {"lr": 0.0001, "epochs": 500, "batch_size": 32},
     },
     {
-        "id": 5,
         "model_name": "Modelo #5",
         "arch": {
             "hidden_units": [128, 128],
-            "activation": tf.keras.activations.relu,
+            "activation": "relu",
         },
         "opt": {"lr": 0.0005, "epochs": 500, "batch_size": 32},
     },
     {
-        "id": 6,
-        "model_name": "PTFlash Classifier",
+        "model_name": "Modelo #6",
         "arch": {
             "hidden_units": [32, 32, 32],
-            "activation": tf.keras.activations.swish,
+            "activation": "relu",
         },
         "opt": {"lr": 0.0005, "epochs": 500, "batch_size": 32},
     },
     {
-        "id": 7,
         "model_name": "Modelo #7",
         "arch": {
-            "hidden_units": [32, 32, 32],
-            "activation": tf.keras.activations.relu,
-        },
-        "opt": {"lr": 0.0005, "epochs": 500, "batch_size": 32},
-    },
-    {
-        "id": 8,
-        "model_name": "Modelo #8",
-        "arch": {
             "hidden_units": [128, 128, 128],
-            "activation": tf.keras.activations.relu,
+            "activation": "relu",
         },
         "opt": {"lr": 0.0001, "epochs": 500, "batch_size": 32},
     },
     {
-        "id": 9,
         "model_name": "ANN-STAB",
         "arch": {
             "hidden_units": [64, 128, 128, 128, 64],
-            "activation": tf.keras.activations.relu,
+            "activation": "relu",
         },
         "opt": {"lr": 0.0001, "epochs": 80, "batch_size": 64},
     },
+    {
+        "model_name": "PTFlash Classifier",
+        "arch": {
+            "hidden_units": [32, 32, 32],
+            "activation": "silu",
+        },
+        "opt": {"lr": 0.0005, "epochs": 500, "batch_size": 32},
+    },
 ]
+
+hparams = [{"id": str(uuid.uuid1()), **hp} for hp in hparams]

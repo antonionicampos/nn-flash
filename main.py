75cf4982b5f7ec40397a0fa745fffff9adb2176a
Antonioni Campos <antonioni.campos@petrobras.com>
2024-07-24 19:45:04 -0300

    add samples_per_composition and analysis arguments

--- main.py
+++ main.py
@@ -35,8 +35,14 @@ neqsim_logger.propagate = False
 logger_params = {"format": "[%(asctime)s] %(levelname)s %(name)s:%(lineno)3d | %(message)s", "filename": logs_filepath}
 
 if __name__ == "__main__":
-    samples_per_composition = 3
-    
+    parser.add_argument(
+        "-s",
+        "--samples-per-composition",
+        default=3,
+        action="store",
+        choices=["3", "30", "300"],
+        help="Select dataset depending on number of P, T samples per composition sample",
+    )
     parser.add_argument(
         "-r",
         "--read",
@@ -52,10 +58,14 @@ if __name__ == "__main__":
         help="Create CV datasets from processed data",
     )
     parser.add_argument("-t", "--training", default=None, action="store_true", help="Do train step")
-    parser.add_argument("-w", "--warning", default=None, action="store_true", help="Run main.py in WARNING mode")
-    parser.add_argument("-v", "--verbose", default=None, action="store_true", help="Run main.py in DEBUG mode")
+    parser.add_argument("-a", "--analysis", default=None, action="store_true", help="Do Analysis Step")
+    parser.add_argument("-w", "--warning", default=None, action="store_true", help="Run in WARNING mode")
+    parser.add_argument("-v", "--verbose", default=None, action="store_true", help="Run in DEBUG mode")
 
     args = parser.parse_args()
+
+    print(args)
+    samples_per_composition = int(args.samples_per_composition)
 
     if args.verbose:
         logging.basicConfig(level=logging.DEBUG, **logger_params)
@@ -72,7 +82,6 @@ if __name__ == "__main__":
     if args.read:
         logger.info("Starting read data")
         data_transform = DataTransform(data_folder)
-
         logger.info("Starting read raw data from JSON file and transform to CSV file")
         data_transform.load_raw_data()
         logger.info("Starting transform CSV raw data")
@@ -83,7 +92,6 @@ if __name__ == "__main__":
     if args.cross_validation:
         logger.info("Starting create cross-validation data")
         cv_data = CrossValidation(data_folder)
-
         logger.info("Starting create classification cross-validation datasets")
         cv_data.create_datasets(model="classification", samples_per_composition=samples_per_composition)
         logger.info("Starting create regression cross-validation datasets")
@@ -91,8 +99,9 @@ if __name__ == "__main__":
 
     if args.training:
         logger.info("Starting training models")
-        run_training()
+        run_training(samples_per_composition=samples_per_composition)
 
+    if args.analysis:
         from pprint import pprint
 
-        pprint(load_training_models())
+        pprint(load_training_models(samples_per_composition=samples_per_composition))

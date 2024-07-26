f2f400627e9f92de7d98e614dd79df834e616032
Antonioni Campos <antonioni.campos@petrobras.com>
2024-07-26 17:42:54 -0300

    add get_config() file to tf.keras.Model

--- src/models/classification/__init__.py
+++ src/models/classification/__init__.py
@@ -4,9 +4,11 @@ import tensorflow as tf
 class NeuralNetClassifier(tf.keras.Model):
     def __init__(self, hidden_units=[32], activation="relu", *args, **kwargs):
         super(NeuralNetClassifier, self).__init__(*args, **kwargs)
+        self.hidden_units = hidden_units
+        self.activation = activation
         self.hiddens = [
-            tf.keras.layers.Dense(units, activation=activation, name=f"dense{i+1}")
-            for i, units in enumerate(hidden_units)
+            tf.keras.layers.Dense(units, activation=self.activation, name=f"dense{i+1}")
+            for i, units in enumerate(self.hidden_units)
         ]
         self.outputs = tf.keras.layers.Dense(3, name="output")
 
@@ -15,13 +17,16 @@ class NeuralNetClassifier(tf.keras.Model):
             inputs = hidden(inputs)
         return self.outputs(inputs)
 
-    def predict(self, inputs, output_type):
-        output_types = ["logit", "probs", "label"]
-        assert output_type in output_types, f"'output_type' parameter must be 'logit', 'probs' or 'label'."
-        outputs = self(inputs)
-        if output_type == "probs":
-            outputs = tf.nn.softmax(outputs)
-        elif output_type == "label":
-            outputs = tf.nn.softmax(outputs)
-            outputs = tf.argmax(outputs, axis=1)
-        return outputs
+    def get_config(self):
+        return {"hidden_units": self.hidden_units, "activation": self.activation}
+
+    # def predict(self, inputs, output_type, **kwargs):
+    #     output_types = ["logit", "probs", "label"]
+    #     assert output_type in output_types, f"'output_type' parameter must be 'logit', 'probs' or 'label'."
+    #     outputs = self(inputs)
+    #     if output_type == "probs":
+    #         outputs = tf.nn.softmax(outputs)
+    #     elif output_type == "label":
+    #         outputs = tf.nn.softmax(outputs)
+    #         outputs = tf.argmax(outputs, axis=1)
+    #     return outputs

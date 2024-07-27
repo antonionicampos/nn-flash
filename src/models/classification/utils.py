fb366a1bb94d1dae2e6551d792d0cc4537bfdf17
Antonioni Campos <antonioni.campos@petrobras.com>
2024-07-27 00:02:10 -0300

    remove training_history function

--- src/models/classification/utils.py
+++ src/models/classification/utils.py
@@ -27,34 +27,6 @@ def preprocessing(data):
     return features, labels
 
 
-def training_history(results: List[List[Dict[str, Any]]], model_id: int):
-    histories = [r["history"] for r in results[model_id]]
-    f, axs = plt.subplots(len(histories), 2, figsize=(7, 15), sharex=True)
-
-    for i, history in enumerate(histories):
-        loss = history.history["loss"]
-        val_loss = history.history["val_loss"]
-        acc = history.history["categorical_accuracy"]
-        val_acc = history.history["val_categorical_accuracy"]
-        actual_epochs = np.arange(1, len(loss) + 1)
-
-        axs[i, 0].plot(actual_epochs, loss, label="loss")
-        axs[i, 0].plot(actual_epochs, val_loss, label="val_loss")
-        axs[i, 0].legend(prop={"size": 8})
-        axs[i, 0].grid()
-        axs[i, 0].set_ylabel("Entropia Cruzada")
-
-        axs[i, 1].plot(actual_epochs, acc, label="loss")
-        axs[i, 1].plot(actual_epochs, val_acc, label="val_loss")
-        axs[i, 1].legend(prop={"size": 8})
-        axs[i, 1].grid()
-        axs[i, 1].axhline(1.0, color="green")
-        axs[i, 1].set_ylabel("Exatidão")
-
-    f.tight_layout()
-    plt.show()
-
-
 def model_parameters_size(model: tf.keras.Model):
     parameters = [params.numpy().flatten().shape[0] for params in model.trainable_variables]
     return np.prod(parameters)

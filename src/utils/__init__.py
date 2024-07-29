27df9d98cdb1e57c58a61128c0216a429647872f
Antonioni Campos <antonioni.campos@petrobras.com>
2024-07-29 19:48:48 -0300

    implement regression target preprocessing

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,6 +1,8 @@
 import neqsim
+import numpy as np
+import pandas as pd
 
-from src.utils.constants import NEQSIM_COMPONENTS, FEATURES_NAMES, P_MIN_MAX, T_MIN_MAX
+from src.utils.constants import NEQSIM_COMPONENTS, FEATURES_NAMES, P_MIN_MAX, T_MIN_MAX, REGRESSION_TARGET_NAMES
 from typing import Dict
 
 
@@ -23,7 +25,17 @@ def create_fluid(composition: Dict[str, float]):
     return fluid
 
 
-def preprocessing(data):
+def preprocessing(data, problem: str):
+    """Preprocessing data for classification or regression problem returns features and labels.
+
+    Parameters
+    ----------
+    problem : str
+        classification or regression problem preprocessing data
+    Returns
+    -------
+        features and labels
+    """
     processed_data = data.copy()
     processed_data[FEATURES_NAMES[:-2]] = processed_data[FEATURES_NAMES[:-2]] / 100.0
 
@@ -33,5 +45,9 @@ def preprocessing(data):
     processed_data["T"] = (processed_data["T"] - T_min) / (T_max - T_min)
 
     features = processed_data[FEATURES_NAMES].copy()
-    labels = pd.get_dummies(processed_data["class"], dtype=np.float32)
+
+    if problem == "classification":
+        labels = pd.get_dummies(processed_data["class"], dtype=np.float32)
+    elif problem == "regression":
+        labels = processed_data[REGRESSION_TARGET_NAMES].copy()
     return features, labels

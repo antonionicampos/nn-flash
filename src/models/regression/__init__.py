27197c9f6ba5fa4b5deb66c1e52af3db1e406eca
Antonioni Campos <antonioni.campos@petrobras.com>
2024-07-29 20:14:00 -0300

    code improvement on regressor neural net and residual neural net w/ configurable neurons

--- src/models/regression/__init__.py
+++ src/models/regression/__init__.py
@@ -2,57 +2,68 @@ import tensorflow as tf
 
 
 class NeuralNet(tf.keras.Model):
-    def __init__(self, hidden_units=[32], activation=tf.keras.activations.relu):
-        super().__init__()
+    def __init__(self, hidden_units, activation, *args, **kwargs):
+        super(NeuralNet, self).__init__(*args, **kwargs)
+        self.hidden_units = hidden_units
+        self.activation = activation
         self.hiddens = [
-            tf.keras.layers.Dense(units, activation=activation, name=f"dense{i+1}")
-            for i, units in enumerate(hidden_units)
+            tf.keras.layers.Dense(units, activation=self.activation, name=f"dense{i+1}")
+            for i, units in enumerate(self.hidden_units)
         ]
-        self.output = tf.keras.layers.Dense(25, name="output")
+        self.outputs = tf.keras.layers.Dense(25, name="output")
 
-    def call(self, x):
-        for layer in self.hiddens:
-            x = layer(x)
-        return self.output(x)
+    def call(self, inputs):
+        for hidden in self.hiddens:
+            inputs = hidden(inputs)
+        return self.outputs(inputs)
+
+    def get_config(self):
+        return {"hidden_units": self.hidden_units, "activation": self.activation}
 
 
 class ResidualBlock(tf.keras.Model):
     def __init__(self, units, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.silu = tf.keras.activations.swish
-
-        self.hidden1 = tf.keras.layers.Dense(units, activation=self.silu)
+        super(ResidualBlock, self).__init__(*args, **kwargs)
+        self.units = units
+        self.hidden1 = tf.keras.layers.Dense(units, activation=tf.keras.activations.swish)
         self.hidden2 = tf.keras.layers.Dense(units)
 
-    def call(self, x):
-        inputs = tf.identity(x)
-        h1 = self.hidden1(inputs)
+    def call(self, inputs):
+        x = tf.identity(inputs)
+        h1 = self.hidden1(x)
         h2 = self.hidden2(h1)
-
-        h2 += x
+        h2 += inputs
         return self.silu(h2)
+
+    def get_config(self):
+        return {"units": self.units}
 
 
 class ResidualNeuralNet(tf.keras.Model):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, hidden_units, activation, *args, **kwargs):
+        super(ResidualNeuralNet, self).__init__(*args, **kwargs)
+        self.hidden_units = hidden_units
+        self.activation = activation
 
         self.hidden1 = tf.keras.layers.Dense(
             units=64,
             activation=tf.keras.activations.swish,
             name="hidden1",
         )
-        self.residual1 = ResidualBlock(64, name="residual1")
-        self.residual2 = ResidualBlock(64, name="residual2")
-        self.residual3 = ResidualBlock(64, name="residual3")
+        self.residuals = [
+            ResidualBlock(units, activation=self.activation, name=f"residual{i+1}")
+            for i, units in enumerate(self.hidden_units)
+        ]
         self.concatenate = tf.keras.layers.Concatenate(name="concatenate")
-        self.output = tf.keras.layers.Dense(25, name="output")
+        self.outputs = tf.keras.layers.Dense(25, name="output")
 
-    def call(self, x):
-        inputs = tf.identity(x)
-        h1 = self.hidden1(inputs)
-        r1 = self.residual1(h1)
-        r2 = self.residual2(r1)
-        r3 = self.residual3(r2)
-        c1 = self.concatenate([r3, x])
-        return self.output(c1)
+    def call(self, inputs):
+        x = tf.identity(inputs)
+        x = self.hidden1(x)
+        for residual in self.residuals:
+            x = residual(x)
+        concat = self.concatenate([x, inputs])
+        return self.outputs(concat)
+
+    def get_config(self):
+        return {"hidden_units": self.hidden_units, "activation": self.activation}

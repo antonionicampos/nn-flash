bb1df5e28f41da014eb742f394714afa206a8b70
Antonioni Campos <antonioni.campos@petrobras.com>
2024-07-29 16:22:32 -0300

    remove commented code snippet

--- src/models/classification/__init__.py
+++ src/models/classification/__init__.py
@@ -10,23 +10,12 @@ class NeuralNetClassifier(tf.keras.Model):
             tf.keras.layers.Dense(units, activation=self.activation, name=f"dense{i+1}")
             for i, units in enumerate(self.hidden_units)
         ]
-        self.outputs = tf.keras.layers.Dense(3, name="output")
+        self.output = tf.keras.layers.Dense(3, name="output")
 
     def call(self, inputs):
         for hidden in self.hiddens:
             inputs = hidden(inputs)
-        return self.outputs(inputs)
+        return self.output(inputs)
 
     def get_config(self):
         return {"hidden_units": self.hidden_units, "activation": self.activation}
-
-    # def predict(self, inputs, output_type, **kwargs):
-    #     output_types = ["logit", "probs", "label"]
-    #     assert output_type in output_types, f"'output_type' parameter must be 'logit', 'probs' or 'label'."
-    #     outputs = self(inputs)
-    #     if output_type == "probs":
-    #         outputs = tf.nn.softmax(outputs)
-    #     elif output_type == "label":
-    #         outputs = tf.nn.softmax(outputs)
-    #         outputs = tf.argmax(outputs, axis=1)
-    #     return outputs

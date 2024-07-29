2bf0f83bbae65590eb384b9815abf55271db2746
Antonioni Campos <antonioni.campos@petrobras.com>
2024-07-29 16:22:16 -0300

    add preprocessing function implementation

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,6 +1,6 @@
 import neqsim
 
-from src.utils.constants import NEQSIM_COMPONENTS
+from src.utils.constants import NEQSIM_COMPONENTS, FEATURES_NAMES, P_MIN_MAX, T_MIN_MAX
 from typing import Dict
 
 
@@ -21,3 +21,17 @@ def create_fluid(composition: Dict[str, float]):
         fluid.addComponent(NEQSIM_COMPONENTS[component], fraction)
     fluid.setMixingRule("classic")  # classic will use binary kij
     return fluid
+
+
+def preprocessing(data):
+    processed_data = data.copy()
+    processed_data[FEATURES_NAMES[:-2]] = processed_data[FEATURES_NAMES[:-2]] / 100.0
+
+    P_min, P_max = P_MIN_MAX
+    T_min, T_max = T_MIN_MAX
+    processed_data["P"] = (processed_data["P"] - P_min) / (P_max - P_min)
+    processed_data["T"] = (processed_data["T"] - T_min) / (T_max - T_min)
+
+    features = processed_data[FEATURES_NAMES].copy()
+    labels = pd.get_dummies(processed_data["class"], dtype=np.float32)
+    return features, labels

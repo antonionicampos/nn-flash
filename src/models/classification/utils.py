9eda9beee7d9ba2585fefae07ca09de342ae5073
Antonioni Campos <antonioni.campos@petrobras.com>
2024-07-29 19:52:36 -0300

    format script text

--- src/models/classification/utils.py
+++ src/models/classification/utils.py
@@ -12,15 +12,12 @@ def model_parameters_size(model: tf.keras.Model):
 
 def binary_classification(model: tf.keras.Model, data: pd.DataFrame, label: int):
     features, labels = preprocessing(data, problem="classification")
-
     X = tf.convert_to_tensor(features)
     y = tf.convert_to_tensor(labels)
     y = tf.argmax(y, axis=1)
 
     logits = model(X)
     probs = tf.nn.softmax(logits)
-
     y_class = np.where(y == label, 1, 0)
     y_hat_class = probs[:, label]
-
     return y_class, y_hat_class

f323191a4be47390da0fc7d41b7998ae1d62c3c7
Antonioni Campos <antonioni.campos@petrobras.com>
2024-07-31 17:09:28 -0300

    add denorm function

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,8 +1,7 @@
 import neqsim
-import numpy as np
 import pandas as pd
 
-from src.utils.constants import NEQSIM_COMPONENTS, FEATURES_NAMES, P_MIN_MAX, T_MIN_MAX, REGRESSION_TARGET_NAMES
+from src.utils.constants import NEQSIM_COMPONENTS
 from typing import Dict
 
 
@@ -25,29 +24,5 @@ def create_fluid(composition: Dict[str, float]):
     return fluid
 
 
-def preprocessing(data, problem: str):
-    """Preprocessing data for classification or regression problem returns features and labels.
-
-    Parameters
-    ----------
-    problem : str
-        classification or regression problem preprocessing data
-    Returns
-    -------
-        features and labels
-    """
-    processed_data = data.copy()
-    processed_data[FEATURES_NAMES[:-2]] = processed_data[FEATURES_NAMES[:-2]] / 100.0
-
-    P_min, P_max = P_MIN_MAX
-    T_min, T_max = T_MIN_MAX
-    processed_data["P"] = (processed_data["P"] - P_min) / (P_max - P_min)
-    processed_data["T"] = (processed_data["T"] - T_min) / (T_max - T_min)
-
-    features = processed_data[FEATURES_NAMES].copy()
-
-    if problem == "classification":
-        labels = pd.get_dummies(processed_data["class"], dtype=np.float32)
-    elif problem == "regression":
-        labels = processed_data[REGRESSION_TARGET_NAMES].copy()
-    return features, labels
+def denorm(data: pd.DataFrame, min_vals: pd.Series, max_vals: pd.Series):
+    return data * (max_vals - min_vals) + min_vals

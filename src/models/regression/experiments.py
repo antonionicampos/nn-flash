0540deb7f4f0979023a15499475bba47b7994d46
Antonioni Campos <antonioni.campos@petrobras.com>
2024-08-23 21:09:49 -0300

    change models_specs to experiments

---
+++ src/models/regression/experiments.py
@@ -0,0 +1,23 @@
+hparams = [
+    {
+        "model_type": "neural_network",
+        "params": {
+            "hidden_layers": [0, 1, 2, 3],
+            "hidden_units": [8, 16, 32, 64, 128],
+            "activation": ["relu"],
+        },
+        "opt": {"lr": 0.001, "epochs": 500, "batch_size": 32},
+    },
+    {
+        "model_name": "Phase-Split [Ref 22]",
+        "model_type": "neural_network",
+        "arch": {"hidden_units": [64, 128, 128, 128, 64], "activation": "relu"},
+        "opt": {"lr": 0.0001, "epochs": 80, "batch_size": 64},
+    },
+    {
+        "model_name": "PTFlash Initializer [Ref 19]",
+        "model_type": "neural_network",
+        "arch": {"hidden_units": [64, 64, 64], "type": "residual"},
+        "opt": {"lr": 0.0005, "epochs": 500, "batch_size": 32},
+    },
+]

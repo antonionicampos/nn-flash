4e913ce4dd33c59c932da2c78c2f7e5099c22851
Antonioni Campos <antonioni.campos@petrobras.com>
2024-08-25 23:03:30 -0300

    change from arch to params

--- src/models/regression/experiments.py
+++ src/models/regression/experiments.py
@@ -11,13 +11,13 @@ hparams = [
     {
         "model_name": "Phase-Split [Ref 22]",
         "model_type": "neural_network",
-        "arch": {"hidden_units": [64, 128, 128, 128, 64], "activation": "relu"},
+        "params": {"hidden_units": [64, 128, 128, 128, 64], "activation": "relu"},
         "opt": {"lr": 0.0001, "epochs": 80, "batch_size": 64},
     },
     {
         "model_name": "PTFlash Initializer [Ref 19]",
         "model_type": "neural_network",
-        "arch": {"hidden_units": [64, 64, 64], "type": "residual"},
+        "params": {"hidden_units": [64, 64, 64], "type": "residual"},
         "opt": {"lr": 0.0005, "epochs": 500, "batch_size": 32},
     },
 ]

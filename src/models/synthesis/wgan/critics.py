4d6774b329d3ed3612bc86b08b4a823bd41923da
Antonioni Campos <antonioni.campos@petrobras.com>
2024-09-04 22:17:50 -0300

    fix critic/generator models implementation to save .keras files

--- src/models/synthesis/wgan/critics.py
+++ src/models/synthesis/wgan/critics.py
@@ -3,8 +3,10 @@ import tensorflow as tf
 
 class MLPCritic(tf.keras.Model):
     def __init__(self, hidden_units, activation="relu", *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.hidden_layers = [tf.keras.layers.Dense(units, activation=activation) for units in hidden_units]
+        super(MLPCritic, self).__init__(*args, **kwargs)
+        self.hidden_units = hidden_units
+        self.activation = activation
+        self.hidden_layers = [tf.keras.layers.Dense(units, activation=self.activation) for units in self.hidden_units]
         self.output_layer = tf.keras.layers.Dense(1)
 
     def call(self, x):
@@ -12,3 +14,6 @@ class MLPCritic(tf.keras.Model):
         for layer in self.hidden_layers:
             hidden = layer(hidden)
         return self.output_layer(hidden)
+
+    def get_config(self):
+        return {"hidden_units": self.hidden_layers, "activation": self.activation}
--- src/models/synthesis/wgan/generators.py
+++ src/models/synthesis/wgan/generators.py
@@ -3,12 +3,18 @@ import tensorflow as tf
 
 class MLPGenerator(tf.keras.Model):
     def __init__(self, output_dim, hidden_units, activation="relu", *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.hidden_layers = [tf.keras.layers.Dense(units, activation=activation) for units in hidden_units]
-        self.output_layer = tf.keras.layers.Dense(output_dim, activation="softmax", name="output")
+        super(MLPGenerator, self).__init__(*args, **kwargs)
+        self.output_dim = output_dim
+        self.hidden_units = hidden_units
+        self.activation = activation
+        self.hidden_layers = [tf.keras.layers.Dense(units, activation=self.activation) for units in self.hidden_units]
+        self.output_layer = tf.keras.layers.Dense(self.output_dim, activation="softmax", name="output")
 
     def call(self, x):
         hidden = tf.identity(x)
         for layer in self.hidden_layers:
             hidden = layer(hidden)
         return self.output_layer(hidden)
+
+    def get_config(self):
+        return {"output_dim": self.output_dim, "hidden_units": self.hidden_units, "activation": self.activation}

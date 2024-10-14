380f68b66b6aae7cb0eeff4564f2b517dfc44758
Antonioni Campos <antonioni.campos@petrobras.com.br>
2024-10-14 15:00:48 -0300

    implement performance_indices_table

--- src/visualization/synthesis/__init__.py
+++ src/visualization/synthesis/__init__.py
@@ -7,7 +7,10 @@ import pandas as pd
 from src.data.handlers import DataLoader
 from src.models.synthesis.train_models import SynthesisTraining
 from src.models.synthesis.evaluate_models import SynthesisAnalysis
+from src.visualization.styles.formatting import errorbar_kwargs
 
+plt.style.use("seaborn-v0_8-paper")
+plt.style.use(os.path.join("src", "visualization", "styles", "l3_mod.mplstyle"))
 
 DPI = 400
 
@@ -33,42 +36,70 @@ class SynthesisViz:
     def models_table(self):
         outputs = self.results["outputs"]
         outputs = [
-            {"model_id": model["model_id"], "model_name": model["model_name"], **model["params"], **model["opt"]}
+            {
+                "model_id": model["model_id"],
+                "model_name": model["model_name"].replace("#", "\#"),
+                **model["params"],
+                # **model["opt"],
+            }
             for model in outputs
+            if model["model_type"] == "wgan"
         ]
-
-        table = pd.DataFrame.from_records(outputs)
+        outputs.insert(0, {"model_id": self.results["outputs"][0]["model_id"], "model_name": "Dirichlet"})
+        table = pd.DataFrame.from_records(outputs).fillna("-")
         table.to_latex(os.path.join(self.viz_folder, "models_table.tex"), index=False)
 
     def performance_indices_table(self):
-        pass
+        model_names = [res["model_name"].replace("#", "\#") for res in self.results["outputs"]]
+        data = {}
+        for name in self.indices.keys():
+            index = self.indices[name]
+            mean, std = index.mean(axis=0), index.std(axis=0) / np.sqrt(self.k_folds - 1)
+            data[name] = [rf"{mu:.4f} \textpm {sigma:.4f}" for mu, sigma in zip(mean, std)]
+        table = pd.DataFrame(data, index=model_names)
+        table.columns = [" ".join([s.capitalize() for s in col.split("_")]) for col in table.columns]
+
+        def highlight(s, props=""):
+            mu = s.apply(lambda row: float(row.split(r" \textpm ")[0]))
+            return np.where(mu == np.min(mu.values), props, "")
+
+        table = table.style.apply(highlight, props="font-weight:bold;", axis=0)
+        table.to_latex(
+            os.path.join(self.viz_folder, "performance_indices_table.tex"),
+            hrules=True,
+            convert_css=True,
+            column_format="lccccc",
+        )
 
     def errorbar_plot(self):
         kl_divergence = self.indices["kl_divergence"]
         wasserstein_dist = self.indices["wasserstein_distance"]
-        models = [model["model_name"].replace("#", "\#") for model in self.results["outputs"]][1:]
+        models = [model["model_name"].replace("#", "\#") for model in self.results["outputs"]]
 
         x = np.arange(len(models))
         y = np.mean(kl_divergence, axis=0)
-        yerr = np.std(kl_divergence, axis=0) / np.sqrt(self.n_folds - 1)
+        yerr = np.std(kl_divergence, axis=0) / np.sqrt(self.k_folds - 1)
 
         f, axs = plt.subplots(2, 1, sharex=True, figsize=(8, 6))
-        axs[0].errorbar(x=x, y=y[1:], yerr=yerr[1:], fmt="", capsize=3)
+        axs[0].errorbar(x=x, y=y, yerr=yerr, **errorbar_kwargs)
         axs[0].grid(True)
-        # axs[0].set_ylabel(r"$\widehat{D}_{\mathrm{KL}}(P|Q)$")
-        axs[0].set_ylabel("KL Divergence")
+        axs[0].set_ylabel(r"$\widehat{D}_{\mathrm{KL}}(P|Q)$")
+        # axs[0].set_ylabel("KL Divergence Estimate")
 
         y = np.mean(wasserstein_dist, axis=0)
-        yerr = np.std(wasserstein_dist, axis=0) / np.sqrt(self.n_folds - 1)
+        yerr = np.std(wasserstein_dist, axis=0) / np.sqrt(self.k_folds - 1)
 
-        axs[1].errorbar(x=x, y=y[1:], yerr=yerr[1:], fmt="", capsize=3)
+        axs[1].errorbar(x=x, y=y, yerr=yerr, **errorbar_kwargs)
         axs[1].grid(True)
-        # axs[1].set_ylabel(r"$\widehat{W}_{1}(\mu,\nu)$")
-        axs[1].set_ylabel("Wasserstein Distance")
+        axs[1].set_ylabel(r"$\widehat{W}_{1}(\mu,\nu)$")
+        # axs[1].set_ylabel("Wasserstein Distance Estimate")
         axs[1].set_xticks(x, models, rotation=90, ha="center")
 
-        f.subplots_adjust(hspace=0.1)
+        # f.subplots_adjust(hspace=0.1)
+        f.tight_layout()
         f.savefig(os.path.join(self.viz_folder, "errorbar_plot.png"), dpi=DPI)
 
     def create(self):
-        pass
+        self.models_table()
+        self.performance_indices_table()
+        self.errorbar_plot()
